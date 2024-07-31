8df5133ea9fd537d03171aa6da0abe96360dab16
Marc Allen <mallen@zoox.com>
2024-07-31 10:45:21 -0700

    Added diffview and increased history limit, automatic tmux window renumbering

--- nvim/init.lua
+++ nvim/init.lua
@@ -910,6 +910,9 @@ require('lazy').setup({
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
   },
+  {
+    'sindrets/diffview.nvim',
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
